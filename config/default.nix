d30311024f1112e2b0e0f551c3211c4e57b30e3b
filippo-ferrando <fferrando@elemento.cloud>
2026-06-22 00:52:26 +0200

    feat: add gesture tracking on right touchpad

--- config/default.nix
+++ config/default.nix
@@ -4,8 +4,22 @@
 
 let
   config = ./.;
+  gesture_module = ../modules/gesture_processor;
 
-  go60_left  = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
-  go60_right = firmware.zmk.override { board = "go60_rh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
+  go60_left  = (firmware.zmk.override { 
+    board = "go60_lh"; 
+    keymap = "${config}/go60.keymap"; 
+    kconfig = "${config}/go60.conf"; 
+  }).overrideAttrs (oldAttrs: {
+    ZMK_EXTRA_MODULES = gesture_module;
+  });
+
+  go60_right = (firmware.zmk.override { 
+    board = "go60_rh"; 
+    keymap = "${config}/go60.keymap"; 
+    kconfig = "${config}/go60.conf"; 
+  }).overrideAttrs (oldAttrs: {
+    ZMK_EXTRA_MODULES = gesture_module;
+  });
 
 in firmware.combine_uf2 go60_left go60_right "go60"
---
+++ modules/gesture_processor/src/input_processor_gesture.c
@@ -0,0 +1,122 @@
+#define DT_DRV_COMPAT zmk_input_processor_gesture
+
+#include <zephyr/device.h>
+#include <zephyr/input/input.h>
+#include <zephyr/kernel.h>
+#include <zmk/behavior.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+struct gesture_config {
+  int32_t x_threshold;
+  int32_t y_threshold;
+  int32_t cooldown_ms;
+  struct zmk_behavior_binding swipe_left;
+  struct zmk_behavior_binding swipe_right;
+  struct zmk_behavior_binding swipe_up;
+  struct zmk_behavior_binding swipe_down;
+};
+
+struct gesture_data {
+  int32_t acc_x;
+  int32_t acc_y;
+  int64_t last_trigger;
+};
+
+static void invoke_behavior(const struct zmk_behavior_binding *binding) {
+  if (!binding || !binding->behavior_dev)
+    return;
+  struct zmk_behavior_binding_event event = {.position = 0,
+                                             .timestamp = k_uptime_get()};
+  zmk_behavior_invoke_binding((struct zmk_behavior_binding *)binding, event,
+                              true);
+  zmk_behavior_invoke_binding((struct zmk_behavior_binding *)binding, event,
+                              false);
+}
+
+static int gesture_process(const struct device *dev,
+                           struct input_event *event) {
+  const struct gesture_config *cfg = dev->config;
+  struct gesture_data *data = dev->data;
+
+  if (event->type != INPUT_EV_REL ||
+      (event->code != INPUT_REL_X && event->code != INPUT_REL_Y)) {
+    return 0; // Ignora tutto ciò che non è movimento X/Y
+  }
+
+  int64_t now = k_uptime_get();
+
+  if (now - data->last_trigger > cfg->cooldown_ms * 2) {
+    data->acc_x = 0;
+    data->acc_y = 0;
+  }
+
+  if (event->code == INPUT_REL_X)
+    data->acc_x += event->value;
+  if (event->code == INPUT_REL_Y)
+    data->acc_y += event->value;
+
+  event->value = 0; // Sopprime il movimento del mouse azzerando il valore
+
+  if (now - data->last_trigger < cfg->cooldown_ms) {
+    return -EINVAL;
+  }
+
+  bool triggered = false;
+
+  if (data->acc_x > cfg->x_threshold) {
+    invoke_behavior(&cfg->swipe_right);
+    triggered = true;
+  } else if (data->acc_x < -cfg->x_threshold) {
+    invoke_behavior(&cfg->swipe_left);
+    triggered = true;
+  } else if (data->acc_y > cfg->y_threshold) {
+    invoke_behavior(&cfg->swipe_down);
+    triggered = true;
+  } else if (data->acc_y < -cfg->y_threshold) {
+    invoke_behavior(&cfg->swipe_up);
+    triggered = true;
+  }
+
+  if (triggered) {
+    data->acc_x = 0;
+    data->acc_y = 0;
+    data->last_trigger = now;
+  }
+
+  return -EINVAL; // Blocca la catena, il mouse non si muoverà
+}
+
+#define GESTURE_BEHAVIOR_INIT(n, prop)                                         \
+  {                                                                            \
+      .behavior_dev =                                                          \
+          DEVICE_DT_GET_OR_NULL(DT_PHANDLE_BY_IDX(DT_DRV_INST(n), prop, 0)),   \
+      .param1 =                                                                \
+          COND_CODE_0(DT_PHA_HAS_CELL_AT_IDX(DT_DRV_INST(n), prop, 0, param1), \
+                      (0), (DT_PHA_BY_IDX(DT_DRV_INST(n), prop, 0, param1))),  \
+      .param2 =                                                                \
+          COND_CODE_0(DT_PHA_HAS_CELL_AT_IDX(DT_DRV_INST(n), prop, 0, param2), \
+                      (0), (DT_PHA_BY_IDX(DT_DRV_INST(n), prop, 0, param2))),  \
+  }
+
+#define GESTURE_INST(n)                                                        \
+  static struct gesture_data gesture_data_##n = {                              \
+      .acc_x = 0,                                                              \
+      .acc_y = 0,                                                              \
+      .last_trigger = 0,                                                       \
+  };                                                                           \
+  static const struct gesture_config gesture_config_##n = {                    \
+      .x_threshold = DT_INST_PROP(n, x_threshold),                             \
+      .y_threshold = DT_INST_PROP(n, y_threshold),                             \
+      .cooldown_ms = DT_INST_PROP(n, cooldown_ms),                             \
+      .swipe_left = GESTURE_BEHAVIOR_INIT(n, swipe_left_behavior),             \
+      .swipe_right = GESTURE_BEHAVIOR_INIT(n, swipe_right_behavior),           \
+      .swipe_up = GESTURE_BEHAVIOR_INIT(n, swipe_up_behavior),                 \
+      .swipe_down = GESTURE_BEHAVIOR_INIT(n, swipe_down_behavior),             \
+  };                                                                           \
+  DEVICE_DT_INST_DEFINE(n, NULL, NULL, &gesture_data_##n, &gesture_config_##n, \
+                        POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,      \
+                        gesture_process);
+
+DT_INST_FOREACH_STATUS_OKAY(GESTURE_INST)
